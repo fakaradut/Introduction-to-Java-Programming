d2e1a79dc59dcc78cb344ba9ba0dbca6978b05be
fakaradut <58641475+fakaradut@users.noreply.github.com>
2019-12-21 21:16:24 +0300

    Add files via upload

---
+++ chapter13/Exercise13_01/GeometricObject.java
@@ -0,0 +1,43 @@
+package chapter13.Exercise13_01;
+
+import java.util.Date;
+
+public abstract class GeometricObject {
+	private String color;
+	private boolean filled;
+	private Date dateCreated;
+
+	public GeometricObject() {
+		this("White", false);
+	}
+
+	public GeometricObject(String color, boolean filled) {
+		this.color = color;
+		this.filled = filled;
+		dateCreated = new Date();
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isFilled() {
+		return filled;
+	}
+
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public String toString() {
+		return "Color: " + color + " Filled: " + filled + "Date created: " + dateCreated;
+	}
+}
