2c324c7f57b6c1b24c972e79ef3aafeae2515194
fakaradut <58641475+fakaradut@users.noreply.github.com>
2020-02-22 01:39:07 +0300

    Add files via upload

---
+++ chapter05/Exercise05_35.java
@@ -0,0 +1,16 @@
+package chapter05;
+
+public class Exercise05_35 {
+
+	public static void main(String[] args) {
+		double sum = 0;
+
+		for (int i = 1; i <= 624; i++) {
+			sum += 1. / (Math.sqrt(i) + Math.sqrt(i + 1));
+		}
+
+		System.out.println(sum);
+
+	}
+	
+}
