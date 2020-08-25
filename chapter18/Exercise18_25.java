2015f8de5cea89c85cbe5699a4b637976cbca356
furkan ahmet karadut <58641475+fakaradut@users.noreply.github.com>
2020-08-25 13:00:17 +0300

    Add files via upload

---
+++ chapter18/Exercise18_25.java
@@ -0,0 +1,29 @@
+package chapter18;
+
+import java.util.Scanner;
+
+public class Exercise18_25 {
+	
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.print("Enter a string: ");
+			String s = input.next();
+			displayPermutation(s);
+		}
+	}
+
+	public static void displayPermutation(String s) {
+		displayPermutation("", s);
+	}
+
+	public static void displayPermutation(String s1, String s2) {
+		if (s2.length() == 0) {
+			System.out.println(s1);
+		}
+
+		for (int i = 0; i < s2.length(); i++) {
+			displayPermutation(s1 + s2.charAt(i), s2.substring(0, i) + s2.substring(i + 1));
+		}
+	}
+	
+}
